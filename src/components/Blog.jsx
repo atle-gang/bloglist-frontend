c86ffb7018284a5ce303b054d43e70152960740d
Atlegang Sethono <atlegangcode@gmail.com>
2025-01-08 15:33:26 +0200

    Refactor component due to the extraction of the blog details

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
+import { BlogDetails } from "./BlogDetails";
 
 const Blog = ({ blog }) => {
   const [isActive, setIsActive] = useState(false);
-  const [likes, setLikes] = useState(0);
+  const [likes, setLikes] = useState(blog.likes);
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
@@ -12,6 +13,11 @@ const Blog = ({ blog }) => {
   };
 
   const buttonLabel = isActive ? "hide" : "view";
+  console.log(likes)
+
+  const saveFunction = () => {
+    setLikes(likes + 1);
+  }
 
   return (
     <div style={blogStyle}>
@@ -22,14 +28,7 @@ const Blog = ({ blog }) => {
       {/* In React, this behavior allows you to conditionally render elements based on the truthiness of an expression. 
       If the expression before && is true, React will evaluate and render the part after &&. If it's false, React will skip rendering entirely. */}
       {isActive && (
-        <div>
-          <p>{blog.url}</p>
-          <p>
-            likes {likes}{" "}
-            <button onClick={() => setLikes(likes + 1)}>like</button>
-          </p>
-          <p>{blog.author}</p>
-        </div>
+        <BlogDetails blog={blog} likes={likes} setLikes={saveFunction} />
       )}
     </div>
   );
