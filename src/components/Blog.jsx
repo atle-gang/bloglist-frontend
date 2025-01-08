7674c42fe12dc80f64d4b080f1b2501bd4ea5762
Atlegang Sethono <atlegangcode@gmail.com>
2025-01-08 11:48:21 +0200

    Add a button to each blog which controls the details about the blog are shown or not

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [isActive, setIsActive] = useState(false);
+  const [likes, setLikes] = useState(0);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+      </div>
+      {isActive ? (
+        <div>
+          <p>{blog.url}</p>
+          <p>
+            likes {likes}{" "}
+            <button onClick={() => setLikes(likes + 1)}>like</button>
+          </p>
+          <p>{blog.author}</p>
+        </div>
+      ) : (
+        <button onClick={() => setIsActive(true)}>view</button>
+      )}
+    </div>
+  );
+};
 
 export { Blog };
