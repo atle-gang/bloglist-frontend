32eb2deaeafffa172cf8149cdcfa9f5f1ff84e90
Atlegang Sethono <atlegangcode@gmail.com>
2025-01-12 12:24:21 +0200

    Update deleteEntry method to use async/await and change variable name to response in update method

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -22,13 +22,13 @@ const create = async (newBlog) => {
 };
 
 const update = async (id, newObject) => {
-  const request = await axios.put(`${baseUrl}/${id}`, newObject);
-  return request.data;
+  const response = await axios.put(`${baseUrl}/${id}`, newObject);
+  return response.data;
 };
 
-const deleteEntry = (id) => {
-  const request = axios.delete(`${baseUrl}/${id}`, newObject);
-  return request.then((response) => response.data);
+const deleteEntry = async (id) => {
+  const response = await axios.delete(`${baseUrl}/${id}`, newObject);
+  return response.data;
 }
 
 export default { getAll, create, setToken, update, deleteEntry };
