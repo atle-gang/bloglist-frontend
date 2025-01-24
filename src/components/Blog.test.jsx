1654e10e242a9f35bddf83bb3f295d4151f5d402
Atlegang Sethono <atlegangcode@gmail.com>
2025-01-24 12:14:05 +0200

    Write test that ensures that if the like button is clicked twice, the event handler is called twice

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -36,3 +36,31 @@ test("clicking the 'view'  displays the blog details", async () => {
   const blogLikes = screen.getByText("likes");
   expect(blogLikes).toBeDefined();
 });
+
+test("clicking the 'likes' button twice, the event handler the component received as props is called twice", async () => {
+  const blog = {
+    title: "Component testing is done with react-testing-library",
+    author: "Renders blog title",
+    url: "www.firstTest.com",
+    likes: 0,
+    user: { id: "mock-user-id"}, // Ensure the blog has a user with an id
+  };
+
+  const mockHandler = vi.fn();
+
+  // Renders Blog with minimal details
+  render(<Blog blog={blog} saveLikeFunction={mockHandler} />);
+
+  // user clicks the view button to show blog details where 'like' button is embedded
+  const user = userEvent.setup();
+  const button = screen.getByText("view");
+  await user.click(button);
+
+  // user clicks 'like' button twice
+  const likesButton = screen.getByText("like");
+  await user.click(likesButton);
+  await user.click(likesButton);
+
+  // show that button was clicked twice
+  expect(mockHandler.mock.calls).toHaveLength(2);
+});
